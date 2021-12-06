9d89ffb19fbab9d7206bcad62832706564aebdc6
shadyStan1 <37553094+shadyStan1@users.noreply.github.com>
2021-12-06 12:08:29 +0530

    added code for jwt implementation

---
+++ jwt-demo/src/main/java/com/jwt/practice/jwtdemo/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.jwt.practice.jwtdemo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+	 @GetMapping
+	    public ResponseEntity reachSecureEndpoint() {
+
+	        return new ResponseEntity("If your are reading this you reached a secure endpoint", HttpStatus.OK);
+	    }
+}
---
+++ jwt-demo/src/main/java/com/jwt/practice/jwtdemo/services/ApplicationUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.jwt.practice.jwtdemo.services;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.jwt.practice.jwtdemo.models.ApplicationUser;
+import com.jwt.practice.jwtdemo.repositories.ApplicationUserRepository;
+
+@Service
+public class ApplicationUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	private ApplicationUserRepository applicationUserRepository;
+//
+//    public ApplicationUserDetailsService(ApplicationUserRepository applicationUserRepository) {
+//        this.applicationUserRepository = applicationUserRepository;
+//    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
+        if (applicationUser == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(applicationUser.getUsername(), applicationUser.getPassword(), new ArrayList());
+    }
+}
